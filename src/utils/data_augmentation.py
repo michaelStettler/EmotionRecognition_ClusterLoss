bb4050fcdf7ee27ed0a5534134e1c1954d71e60a
Jannik Smidt <js343@hdm-stuttgart.de>
2020-10-07 13:07:55 +0200

    data_augmentation

---
+++ src/utils/data_augmentation.py
@@ -0,0 +1,131 @@
+from tensorflow.keras.preprocessing.image import *
+
+
+def get_data_generator(dataset_parameters, train=True):
+
+    # No Data augmentation
+    if not dataset_parameters['data_augmentation']:
+        data_generator = ImageDataGenerator(
+            rescale=1. / 255,
+            horizontal_flip=False)
+
+    # random horizontal flips, rotation and size changes
+    elif 1 in dataset_parameters['data_augmentation']:
+        if train:
+            data_generator = ImageDataGenerator(
+                rescale=1. / 255,
+                horizontal_flip=True,
+                fill_mode="nearest",
+                zoom_range=0.3,
+                width_shift_range=0.1,
+                height_shift_range=0.1,
+                rotation_range=30)
+        else:
+            data_generator = ImageDataGenerator(
+                rescale=1. / 255,
+                horizontal_flip=False,
+                fill_mode="nearest",
+                zoom_range=0.,
+                width_shift_range=0.,
+                height_shift_range=0.,
+                rotation_range=0.)
+
+    elif dataset_parameters['data_augmentation'] == '2':
+        def preprocess_fn(x):
+            mean_rgb = dataset_parameters['data_augmentation']['mean_rgb']
+            std_rgb = dataset_parameters['data_augmentation']['std_rgb']
+
+            x[:, :, 0] = (x[:, :, 0] - mean_rgb[0]) / std_rgb[0]
+            x[:, :, 1] = (x[:, :, 1] - mean_rgb[1]) / std_rgb[1]
+            x[:, :, 2] = (x[:, :, 2] - mean_rgb[2]) / std_rgb[2]
+            return x
+
+        if train:
+            data_generator = ImageDataGenerator(
+                # set input mean to 0 over the dataset
+                featurewise_center=False,
+                # set each sample mean to 0
+                samplewise_center=False,
+                # divide inputs by std of dataset
+                featurewise_std_normalization=False,
+                # divide each input by its std
+                samplewise_std_normalization=False,
+                # apply ZCA whitening
+                zca_whitening=False,
+                # epsilon for ZCA whitening
+                zca_epsilon=1e-06,
+                # randomly rotate images in the range (deg 0 to 180)
+                rotation_range=30,
+                # randomly shift images horizontally
+                width_shift_range=0.1,
+                # randomly shift images vertically
+                height_shift_range=0.1,
+                # set range for random shear
+                shear_range=0.,
+                # set range for random zoom
+                zoom_range=0.3,
+                # set range for random channel shifts
+                channel_shift_range=0.,
+                # set mode for filling points outside the input boundaries
+                fill_mode='nearest',
+                # value used for fill_mode = "constant"
+                cval=0.,
+                # randomly flip images
+                horizontal_flip=True,
+                # randomly flip images
+                vertical_flip=False,
+                # set rescaling factor (applied before any other transformation)
+                rescale=1./255,
+                # set function that will be applied on each input
+                preprocessing_function=preprocess_fn,
+                # fraction of images reserved for validation
+                # (strictly between 0 and 1)
+                validation_split=0.0)
+        else:
+            data_generator = ImageDataGenerator(
+                # set input mean to 0 over the dataset
+                featurewise_center=False,
+                # set each sample mean to 0
+                samplewise_center=False,
+                # divide inputs by std of dataset
+                featurewise_std_normalization=False,
+                # divide each input by its std
+                samplewise_std_normalization=False,
+                # apply ZCA whitening
+                zca_whitening=False,
+                # epsilon for ZCA whitening
+                zca_epsilon=1e-06,
+                # randomly rotate images in the range (deg 0 to 180)
+                rotation_range=0,
+                # randomly shift images horizontally
+                width_shift_range=0.,
+                # randomly shift images vertically
+                height_shift_range=0.,
+                # set range for random shear
+                shear_range=0.,
+                # set range for random zoom
+                zoom_range=0.,
+                # set range for random channel shifts
+                channel_shift_range=0.,
+                # set mode for filling points outside the input boundaries
+                fill_mode='nearest',
+                # value used for fill_mode = "constant"
+                cval=0.,
+                # randomly flip images
+                horizontal_flip=False,
+                # randomly flip images *
+                vertical_flip=False,
+                # set rescaling factor (applied before any other transformation)
+                rescale=1./255,
+                # set function that will be applied on each input
+                preprocessing_function=preprocess_fn,
+                # fraction of images reserved for validation
+                # (strictly between 0 and 1)
+                validation_split=0.0)
+
+    else:
+        raise ValueError('Wrong data_processing augmentation selection - '
+                         '%s doest not exist'
+                         % dataset_parameters['data_augmentation'])
+
+    return data_generator
--- src/utils/generators.py
+++ src/utils/generators.py
@@ -1,10 +1,20 @@
+import os
+import sys
 import tensorflow as tf
+import pandas as pd
 from tensorflow.keras.preprocessing.image import *
 from sklearn.datasets import make_blobs
+from tensorflow.python.keras.preprocessing.image_dataset import \
+    image_dataset_from_directory
+
+sys.path.insert(0, './')
+from data_augmentation import *
 
 
 def get_generator(dataset_parameters, model_parameters):
     num_classes = dataset_parameters['num_classes']
+    training_data_generator = get_data_generator(dataset_parameters)
+    validation_data_generator = get_data_generator(dataset_parameters, False)
 
     if dataset_parameters['dataset_name'] == 'cifar10':
         # load the images from keras
@@ -17,9 +27,6 @@ def get_generator(dataset_parameters, model_parameters):
         test_label = tf.keras.utils. \
             to_categorical(test_label, num_classes)
 
-        training_data_generator = ImageDataGenerator(rescale=1. / 255)
-        validation_data_generator = ImageDataGenerator(rescale=1. / 255)
-
         # create generators for easier use in model.fit()
         training_generator = training_data_generator.flow(
             training_images,
@@ -29,6 +36,67 @@ def get_generator(dataset_parameters, model_parameters):
         validation_generator = validation_data_generator.flow(
             test_images,
             test_label
+        )
+
+    elif 'csv' in dataset_parameters['labels_type']:
+        training_dataframe = pd.read_csv(
+            model_parameters['csv_training_file'])
+        validation_dataframe = pd.read_csv(
+            model_parameters['csv_validation_file'])
+
+        training_generator = training_data_generator.flow_from_dataframe(
+            dataframe=training_dataframe,
+            directory=dataset_parameters['training_directory'],
+            x_col='subDirectory_filePath',
+            y_col=dataset_parameters['class_label'],
+            has_ext=True,
+            class_mode='categorical',
+            target_size=(model_parameters['image_height'],
+                         model_parameters['image_width']),
+            batch_size=model_parameters['batch_size'],
+            shuffle=True
+        )
+
+        validation_generator = validation_data_generator.flow_from_dataframe(
+            dataframe=validation_dataframe,
+            directory=dataset_parameters['validation_directory'],
+            x_col='subDirectory_filePath',
+            y_col=dataset_parameters['class_label'],
+            has_ext=True,
+            class_mode='categorical',
+            target_size=(model_parameters['img_height'],
+                         model_parameters['img_width']),
+            batch_size=model_parameters['batch_size'],
+            shuffle=False,
+        )
+
+    elif 'directory' in dataset_parameters['labels_type']:
+        training_directory = dataset_parameters['training_directory']
+        validation_directory = dataset_parameters['validation_directory']
+
+        if not os.path.isdir(training_directory):
+            raise ValueError('Training directory does not exist',
+                             training_directory)
+        if not os.path.isdir(validation_directory):
+            raise ValueError('Validation directory does not exist',
+                             validation_directory)
+
+        training_generator = image_dataset_from_directory(
+            training_directory,
+            label_mode='categorical',
+            class_names=dataset_parameters['class_names'],
+            batch_size=model_parameters['batch_size'],
+            image_size=(model_parameters['img_height'],
+                        model_parameters['img_width']),
+            shuffle=True,
+        )
+        validation_generator = image_dataset_from_directory(
+            validation_directory,
+            label_mode='categorical',
+            class_names=dataset_parameters['class_names'],
+            image_size=(model_parameters['img_height'],
+                        model_parameters['img_width']),
+            shuffle=True,
         )
 
     # not working? blob are can only be created as shape (samples, features)
@@ -51,4 +119,3 @@ def get_generator(dataset_parameters, model_parameters):
         validation_generator = [test_data, test_label]
 
     return training_generator, validation_generator
-
